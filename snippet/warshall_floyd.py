8748754d22b8224ddb6d2b32febef15511ae6a1b
Sekine <ktaoblige@gmail.com>
2020-04-05 08:09:32 +0900

    modify warshall initialize

--- snippet/warshall_floyd.py
+++ snippet/warshall_floyd.py
@@ -1,20 +1,45 @@
-# https://atcoder.jp/contests/abc079/tasks/abc079_d
+# https://atcoder.jp/contests/abc022/tasks/abc022_c
 
-from collections import Counter
-from functools import reduce
+from itertools import product
 
-# 行列(パス無し)は inf で初期化
 inf = float('INF')
-def warshall(d, n):
+def warshall_floyd(d, n):
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if d[i][k] + d[k][j] < d[i][j]:
                     d[i][j] = d[i][k] + d[k][j]
+    # [print(i) for i in d]
 
-h,w = map(int,input().split())
-magic = [list(map(int,input().split())) for _ in range(10)]
-counter = Counter()
-[counter.update(Counter(map(int, input().split()))) for _ in range(h)]
-warshall(magic, 10)
-print(reduce(lambda x,y:x+y, [magic[k][1] * v for k,v in counter.items() if k > -1]))
+def initialize_warshall(n):
+    g = [[inf for i in range(n)] for i in range(n)]
+    for i in range(n):
+        g[i][i] = 0
+    return g
+
+n,m = map(int,input().split())
+graph = initialize_warshall(n)
+
+start = {}
+for i in range(m):
+    u,v,l = map(int,input().split())
+    u -= 1
+    v -= 1
+    if 0 == u:
+        start[v] = l
+    elif 0 == v:
+        start[u] = l
+    else:
+        graph[u][v] = graph[v][u] = l
+
+warshall_floyd(graph, n)
+ans = inf
+flag = False
+for u,v in product(start.keys(), repeat=2):
+    if u == v:
+        continue
+    cost = graph[u][v] + start[u] + start[v]
+    if ans > cost:
+        ans = cost
+        flag = True
+print(ans if flag else -1)
