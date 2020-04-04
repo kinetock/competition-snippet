b460773ecda872b35163abe554355a057181e06c
Sekine <ktaoblige@gmail.com>
2020-04-05 08:09:07 +0900

    modify lis

--- snippet/lis.py
+++ snippet/lis.py
@@ -1,14 +1,19 @@
+# https://atcoder.jp/contests/abc038/tasks/abc038_d
+
 import bisect
 
-def lis(n, A):
-    L = [0 for i in range(n + 1)]
-    L[0] = A[0]
-    length = 1
-    for i in range(1, n):
-        if L[length - 1] < A[i]:
-            L[length] = A[i]
-            length += 1
+def lis(a, n):
+    seq = [a[0][1]]
+    for w,h in a[1:]:
+        if h > seq[-1]:
+            seq.append(h)
         else:
-            index = bisect.bisect_left(L, A[i], 0, length)
-            L[index] = A[i]
-    return length
+            j = bisect.bisect_left(seq, h)
+            seq[j] = h
+        # print(seq, (w,h))
+    return len(seq)
+
+n = int(input())
+a = sorted([list(map(int, input().split())) for _ in range(n)], key=lambda x:(x[0], -x[1]))
+
+print(lis(a,n))
